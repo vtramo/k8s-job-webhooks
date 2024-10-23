3d8d6e3d273de7c4821e017f4e5dcab1095819e2
Vincenzo Tramo <vincenzo@fervento.com>
2024-10-23 10:35:57 +0200

    fix: parse job family watchers config file before start http server

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use k8s_job_webhooks::{service, setup};
 async fn main() -> anyhow::Result<()> {
     setup::init_logging()?;
     setup::init_database().await?;
+    if let Err(_) = setup::parse_job_family_watchers_config_file().await {}
     service::k8s_job_watcher::spawn_k8s_job_watcher();
     setup::init_http_server().await?;
-    setup::parse_job_family_watchers_config_file().await?;
     Ok(())
 }
